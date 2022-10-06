855777df756fd3084ed0d8cc6378eb0683f34c8e
Ferney021 <eseguragalindo@gmail.com>
2022-10-05 21:03:43 -0500

    Updating Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,10 @@ RUN npm install
 COPY . .
 EXPOSE 1337
 CMD ["node", "app.js"]
+
+# docker build . -t inscription_ms
+# docker run --name inscription_ms -p 1337:1337 -d inscription_ms
+
+# DATABASE
+# docker run -d -p 3305:3306 --name inscription-db -e MYSQL_ROOT_PASSWORD=securepassword mysql
+# docker exec -it inscription-db mysql -p
--- config/datastores.js
+++ config/datastores.js
@@ -49,7 +49,7 @@ module.exports.datastores = {
     *                                                                          *
     ***************************************************************************/
     adapter: 'sails-mysql',
-    url: 'mysql://development:siaunal2022@localhost:3305/inscription_db',
+    url: 'mysql://development:siaunal2022@172.17.0.2:3306/inscription_db',
 
   },
 
